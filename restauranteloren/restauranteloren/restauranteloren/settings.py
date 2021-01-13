6e6f565cb7586c1d3c5ed1759d6ac7a0eaafb0d6
admin <arekarino@gmail.com>
2021-01-13 10:57:42 +0000

    edit database

--- restauranteloren/restauranteloren/restauranteloren/settings.py
+++ restauranteloren/restauranteloren/restauranteloren/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'restauranteloren.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tpvloren',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Madrid'
 
 USE_I18N = True
 
